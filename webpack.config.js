e1aaf0bc67ea733593ca38b8152923a0ad6f8ca1
Koki <enoch.binas@gmail.com>
2023-04-15 23:21:22 +0800

    environment setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+
+  devtool: 'inline-source-map',
+};
